e4905831da1cf0752b16ff3967b10381dc032747
Miguel Prada <mapra99@gmail.com>
2020-04-13 00:33:42 -0500

    implemented player class. Added method to validate amount of frames

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -2,10 +2,16 @@
 
 class Models::Player
   attr_accessor :name
-  attr_accessor :pinfalls
+  attr_reader :pinfalls
 
   def initialize(name, pinfalls)
-    @name = name
-    @pinfalls = pinfalls
+    self.name = name
+    self.pinfalls = pinfalls
+  end
+
+  def pinfalls=(new_pinfalls)
+    raise ArgumentError, 'Only 10 Frames are allowed' if new_pinfalls.length != 10
+
+    @pinfalls = new_pinfalls
   end
 end
