427f5f472eea87bb14e19f751e5f9ef1200c852e
Miguel Prada <mapra99@gmail.com>
2020-04-13 02:44:54 -0500

    implemented player class

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -14,4 +14,26 @@ class Models::Player
 
     @pinfalls = new_pinfalls
   end
+
+  def scores
+    result = Array.new 10, 0
+
+    pinfalls.each_with_index do |p, i|
+      if p.strike? && i < 8
+        result[i] = if pinfalls[i + 1].strike?
+                      10 + pinfalls[i + 1].launches[0].value + pinfalls[i + 2].launches[0].value + result[i - 1]
+                    else
+                      10 + pinfalls[i + 1].launches.inject(0) { |s, l| s + l.value } + result[i - 1]
+                    end
+      elsif p.strike? && i == 8
+        result[i] = 10 + pinfalls[i + 1].launches[0].value + pinfalls[i + 1].launches[1].value + result[i - 1]
+      elsif p.spare? && i < 9
+        result[i] = 10 + pinfalls[i + 1].launches[0].value + result[i - 1]
+      else
+        result[i] = p.launches.inject(0) { |s, l| s + l.value } + result[i - 1]
+      end
+    end
+
+    result
+  end
 end
